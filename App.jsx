4b873eae658e25821fa36135c970f511f31277e3
sangeethsahana <71549426+sangeethsahana@users.noreply.github.com>
2021-06-07 02:36:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Node";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
